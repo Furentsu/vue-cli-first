ab957c025a0bbe9912b3096c3d929d52fd7900f6
Furentsu <eugeniofranzoso@gmail.com>
2021-09-23 16:56:06 +0200

    Added Title

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="../assets/ocean-3605547_1920.jpg" alt="Ocean's image by night">
+</template>
+
+<script>
+export default {
+    name: 'Hero',
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
